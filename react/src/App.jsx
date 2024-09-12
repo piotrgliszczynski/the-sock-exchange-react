98fa8e5e7c7ac6abb2aa4cd53ff904831c330c19
Piotr Gliszczynski <piotr.gliszczynski@adp.com>
2024-09-12 11:28:26 -0400

    Added new components

--- react/src/App.jsx
+++ react/src/App.jsx
@@ -1,6 +1,10 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
+import Sock from './components/Sock'
+import Footer from './components/Footer'
+import Search from './components/Search'
+import sock_data from './assets/sock.json'
 import './App.css'
 
 function App() {
@@ -37,10 +41,7 @@ function App() {
                 <a className="nav-link disabled" aria-disabled="true">Disabled</a>
               </li>
             </ul>
-            <form className="d-flex" role="search">
-              <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search" />
-              <button className="btn btn-outline-success" type="submit">Search</button>
-            </form>
+            <Search />
           </div>
         </div>
       </nav>
@@ -49,7 +50,10 @@ function App() {
         <div className="container-fluid">
           <div className="row">
             Both socks and space rockets 🚀 will take you to new heights, but only one will get cold feet!
-            <div className="card-container"></div>
+            <div className="card-container">
+              <Sock data={sock_data} />
+              <Footer environment="Development" />
+            </div>
           </div>
         </div>
       </main>
---
+++ react/src/components/Sock.jsx
@@ -0,0 +1,32 @@
+import React from 'react';
+
+const Sock = (props) => {
+  const convertBoolean = (prop) => {
+    return prop ? 'No' : 'Yes';
+  }
+
+  return (
+    <div className="card">
+      <div className="card-body">
+        <h5 className="card-title">Sock Details</h5>
+        <div className="card-text">Size: {props.data.sockDetails.size}</div>
+        <div className="card-text">Color: {props.data.sockDetails.color}</div>
+        <div className="card-text">Pattern: {props.data.sockDetails.pattern}</div>
+        <div className="card-text">Material: {props.data.sockDetails.material}</div>
+        <div className="card-text">Condition: {props.data.sockDetails.condition}</div>
+        <div className="card-text">For Foot: {props.data.sockDetails.forFoot} </div>
+      </div>
+      <div className="card-body">
+        <h5 className="card-title">Additional features</h5>
+        <div className="card-text">Water Resistant: {convertBoolean(props.data.additionalFeatures.waterResistant)}</div>
+        <div className="card-text">Padded: {convertBoolean(props.data.additionalFeatures.padded)}</div>
+        <div className="card-text">Anti Bacterial: {convertBoolean(props.data.additionalFeatures.antiBacterial)}</div>
+      </div>
+      <div className="card-footer">
+        <small className="text-muted">Added: {props.data.addedTimestamp}</small>
+      </div>
+    </div>
+  )
+}
+
+export default Sock;
